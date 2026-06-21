27564db8ba453bc391b35e262b008787a7b8110d
alin26 <alin.joseph.shan25@gmail.com>
2026-06-21 13:08:29 +0530

    Added FirestoreServices with full CRUD

---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,125 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:manorama_delivery/models/book.dart';
+import 'package:manorama_delivery/models/delivery.dart';
+import 'package:manorama_delivery/models/group.dart';
+import 'package:manorama_delivery/models/shop.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Stream<List<Shop>> getShops() {
+    return _db
+        .collection('shops')
+        .snapshots()
+        .map(
+          (snap) => snap.docs
+              .map((docs) => Shop.fromFirestore(docs.data(), docs.id))
+              .toList(),
+        );
+  }
+
+  Future<void> addShop(Shop shop) async {
+    await _db.collection('shops').add(shop.toFirestore());
+  }
+
+  Future<void> updateShop(Shop shop) async {
+    await _db.collection('shops').doc(shop.id).update(shop.toFirestore());
+  }
+
+  Future<void> deleteShop(String shopId) async {
+    await _db.collection('shops').doc(shopId).delete();
+  }
+
+  Stream<List<ShopGroup>> getGroups() {
+    return _db
+        .collection('groups')
+        .snapshots()
+        .map(
+          (snaps) => snaps.docs
+              .map((doc) => ShopGroup.fromFirestore(doc.data(), doc.id))
+              .toList(),
+        );
+  }
+
+  Future<void> addGroup(ShopGroup group) async {
+    await _db.collection('groups').add(group.toFirestore());
+  }
+
+  Future<void> updateGroup(ShopGroup group) async {
+    await _db.collection('groups').doc(group.id).update(group.toFirestore());
+  }
+
+  Future<void> deleteGroup(String groupId) async {
+    await _db.collection('groups').doc(groupId).delete();
+  }
+
+  Stream<List<Book>> getBooks() {
+    return _db
+        .collection('books')
+        .snapshots()
+        .map(
+          (snaps) => snaps.docs
+              .map((e) => Book.fromFirestore(e.data(), e.id))
+              .toList(),
+        );
+  }
+
+  Future<void> addBook(Book book) async {
+    await _db.collection('books').add(book.toFirestore());
+  }
+
+  Future<void> updateBook(Book book) async {
+    await _db.collection('books').doc(book.id).update(book.toFirestore());
+  }
+
+  Future<void> deleteBook(String bookId) async {
+    await _db.collection('books').doc(bookId).delete();
+  }
+
+  Stream<List<Delivery>> getDeliveries(String shopId, String type) {
+    return _db
+        .collection('deliveries')
+        .where('shopId', isEqualTo: shopId)
+        .where('type', isEqualTo: type)
+        .orderBy('deliveryDate', descending: true)
+        .snapshots()
+        .map(
+          (snaps) => snaps.docs
+              .map((e) => Delivery.fromFirestore(e.data(), e.id))
+              .toList(),
+        );
+  }
+
+  Future<void> addDelivery(Delivery delivery) async {
+    await _db.collection('deliveries').add(delivery.toFirestore());
+  }
+
+  Future<void> updateDelivery(Delivery delivery) async {
+    await _db
+        .collection('deliveries')
+        .doc(delivery.id)
+        .update(delivery.toFirestore());
+  }
+
+  Future<void> deleteDelivery(String deliveryId) async {
+    await _db.collection('deliveries').doc(deliveryId).delete();
+  }
+
+  Stream<List<Delivery>> getDeliveriesByDateRange(
+    DateTime start,
+    DateTime end,
+    String type,
+  ) {
+    return _db
+        .collection('deliveries')
+        .where("type", isEqualTo: type)
+        .where('deliveryDate', isGreaterThanOrEqualTo: start.toIso8601String())
+        .where('deliveryDate', isLessThanOrEqualTo: end.toIso8601String())
+        .snapshots()
+        .map(
+          (snaps) => snaps.docs
+              .map((e) => Delivery.fromFirestore(e.data(), e.id))
+              .toList(),
+        );
+  }
+}
